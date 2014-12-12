775a088e28a728284420fedac62e7b849d8fbdc8
tjcnrl <tjcnrl@c3fc86fe-82d8-4841-a663-0ee342cebea3>
2014-12-12 17:06:36 +0000

    Reconcile VdaDriver with main COAMPS version.

--- NUOPC/team/tjcnrl/COAMPS/COAMPS_VdaDriver.F90
+++ NUOPC/team/tjcnrl/COAMPS/COAMPS_VdaDriver.F90
@@ -6,6 +6,7 @@
 !-------------------------------------------------------------------------------
 #define FILENAME "COAMPS_VdaDriver.F90"
 #include "COAMPS_Macros.h"
+#define USE_RUN_PREP_POST
 
 
 program mainApp
@@ -18,46 +19,86 @@ program mainApp
 
   implicit none
 
+  !! ESMF driver variables
   integer                 :: rc, urc
   type(ESMF_GridComp)     :: gcomp_ad, gcomp_rp
   integer                 :: argCount
   type(ESMF_Config)       :: config
   character(ESMF_MAXSTR)  :: configFile
-  character(ESMF_MAXSTR)  :: msgString
   type(ESMF_VM)           :: vm
+  character(ESMF_MAXSTR)  :: msgString
   integer                 :: petCount, localPet
   integer, parameter      :: rootPet = 0
-  logical                 :: atm_active, ocn_active, wav_active
-  integer, parameter      :: num_cg_iterations = 5
-  integer                 :: iter
   integer     , parameter :: run_prep_phase = 2
   integer     , parameter :: run_post_phase = 3
 
-  ! Initialize ESMF
+  !! configuration file variables
+  logical                 :: atm_active, ocn_active, wav_active
+
+  !! ocean observation variables
+  integer                 :: n_ocn
+  real                    :: oc_inv,oc_val,oc_err
+  real                    :: oc_xi,oc_yj,oc_lvl
+  real                    :: oc_lat,oc_lon
+  integer,allocatable     :: ocn_dtg(:),ocn_tme( :)
+  integer,allocatable     :: ocn_var(:),ocn_typ( :)
+  real(8),allocatable     :: ocn_inv(:),ocn_err( :)
+  real(8),allocatable     :: ocn_cgp(:),ocn_cgAp(:)
+  real(8),allocatable     :: ocn_xi( :),ocn_yj(  :)
+  real(8),allocatable     :: ocn_lvl(:)
+
+  !! atmospheric observation variables (more added later)
+  integer                 :: n_atm
+
+  !! wave observation variables (more added later)
+  integer                 :: n_wav
+
+  !! Conjugate gradient variables
+  integer, parameter      :: num_cg_itr = 5
+  integer                 :: n_obs,cg_itr
+  real(8),allocatable     :: beta(:),cgAp(:),cgr(:)
+  real(8),allocatable     :: cgp( :),cgp1(:),err(:)
+  real(8)                 :: cg_rzOld,cg_beta
+  real(8)                 :: cg_alpha,cg_omega
+  real(8)                 :: cg_bsum,cg_rz
+  real(8)                 :: cg_rAz,cg_rho
+
+  !! local variables
+  integer,parameter       :: iounit=1005
+  integer                 :: i,j,k,ob,n_data
+  integer                 :: ana_dtg
+  character(ESMF_MAXSTR)  :: fname,sfx,cenv,anadtg
+  character(ESMF_MAXSTR)  :: obs_dir,io_dir
+
+!!===================================================================================!!
+!!                          ESMF SETUP FUNCTIONS                                     !!
+!!===================================================================================!!
+
+  !! Initialize ESMF
   call ESMF_Initialize(logkindflag=ESMF_LOGKIND_MULTI, &
     defaultCalkind=ESMF_CALKIND_GREGORIAN, rc=rc)
   if (ESMF_LogFoundError(rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
 
-  ! Set log defaults
+  !! Set log defaults
   call ESMF_LogSet(flush=.true.)
 
-  ! Get VM info
+  !! Get VM info
   call ESMF_VMGetGlobal(vm, rc=rc)
   if (ESMF_LogFoundError(rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
   call ESMF_VMGet(vm, localPet=localPet, petCount=petCount, rc=rc)
   if (ESMF_LogFoundError(rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
 
-  ! Add required fields to NUOPC field dictionary
+  !! Add required fields to NUOPC field dictionary
   call InitFieldDictionary(rc=rc)
   if (ESMF_LogFoundError(rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
 
-  ! Create the driver Components
+  !! Create the driver Components
   gcomp_ad = ESMF_GridCompCreate(name='COAMPS_AD', rc=rc)
   if (ESMF_LogFoundError(rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
   gcomp_rp = ESMF_GridCompCreate(name='COAMPS_RP', rc=rc)
   if (ESMF_LogFoundError(rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
 
-  ! Create & set config for the driver Components
+  !! Create & set config for the driver Components
   configFile = 'coamps.rc'
   call ESMF_UtilGetArgC(argCount, rc=rc)
   if (ESMF_LogFoundError(rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
@@ -74,23 +115,7 @@ program mainApp
   call ESMF_GridCompSet(gcomp_rp, config=config, rc=rc)
   if (ESMF_LogFoundError(rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
 
-  ! SetServices for the driver Components
-  call ESMF_GridCompSetServices(gcomp_ad, drmSS_ad, userRc=urc, rc=rc)
-  if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-  if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-  call ESMF_GridCompSetServices(gcomp_rp, drmSS_rp, userRc=urc, rc=rc)
-  if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-  if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-
-  ! Initialize the driver Components
-  call ESMF_GridCompInitialize(gcomp_ad, userRc=urc, rc=rc)
-  if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-  if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-  call ESMF_GridCompInitialize(gcomp_rp, userRc=urc, rc=rc)
-  if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-  if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-
-  ! Get model active flags from config
+  !! Get model active flags from config
   call ESMF_ConfigGetAttribute(config, atm_active, default=.false., &
        label='ATM_active:', rc=rc)
   if (ESMF_LogFoundError(rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
@@ -101,14 +126,40 @@ program mainApp
        label='WAV_active:', rc=rc)
   if (ESMF_LogFoundError(rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
 
-  !!! ***** CG set-up will go here; begin CG loop ***** !!!
+  !! SetServices for the AD & TL driver Components
+  call ESMF_GridCompSetServices(gcomp_ad, drmSS_ad, userRc=urc, rc=rc)
+  if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  call ESMF_GridCompSetServices(gcomp_rp, drmSS_rp, userRc=urc, rc=rc)
+  if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+#ifdef USE_RUN_PREP_POST
+  !! Initialize for the AD & TL driver Components
+  call ESMF_GridCompInitialize(gcomp_ad, userRc=urc, rc=rc)
+  if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  call ESMF_GridCompInitialize(gcomp_rp, userRc=urc, rc=rc)
+  if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+#endif
+
+!!===================================================================================!!
+!!                       Obtain observations and set-up CG loop                      !!
+!!===================================================================================!!
+
+  !! Print active flags
   if (localPet.eq.rootPet) then
     write (*,'(a,3l2)') 'model active flags: ',atm_active,ocn_active,wav_active
   endif
 
-  do iter = 1,num_cg_iterations
+!!===================================================================================!!
+!!                           Begin CG Minimization Loop                              !!
+!!===================================================================================!!
 
-     write(msgString,'(a,1i6)') 'CG iteration: ',iter
+  cg_iteration_loop: do cg_itr = 1,num_cg_itr
+
+     write(msgString,'(a,1i6)') 'CG iteration: ',cg_itr
      call ESMF_LogWrite('', ESMF_LOGMSG_INFO)
      call ESMF_LogWrite('', ESMF_LOGMSG_INFO)
      call ESMF_LogWrite(trim(msgString), ESMF_LOGMSG_INFO)
@@ -116,49 +167,83 @@ program mainApp
 
   !!! ***** Begin matrix multiplication (action of adjoint and TLM) ***** !!!
 
-     ! Run the adjoint driver Component
-     call ESMF_GridCompRun(gcomp_ad, phase=run_prep_phase, userRc=urc, rc=rc)
-     if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-     if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-     call ESMF_GridCompRun(gcomp_ad, userRc=urc, rc=rc)
-     if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-     if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-     call ESMF_GridCompRun(gcomp_ad, phase=run_post_phase, userRc=urc, rc=rc)
-     if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-     if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  !!! ***** Run the adjoint model ***** !!!
+#ifdef USE_RUN_PREP_POST
+       ! Call RunPrep for the adjoint driver Component
+       call ESMF_GridCompRun(gcomp_ad, phase=run_prep_phase, userRc=urc, rc=rc)
+#else
+       ! Call Initialize for the adjoint driver Component
+       call ESMF_GridCompInitialize(gcomp_ad, userRc=urc, rc=rc)
+#endif
+       if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+       if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+       ! Call Run     for the adjoint driver Component
+       call ESMF_GridCompRun(gcomp_ad, userRc=urc, rc=rc)
+       if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+       if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+#ifdef USE_RUN_PREP_POST
+       ! Call RunPost for the adjoint driver Component
+       call ESMF_GridCompRun(gcomp_ad, phase=run_post_phase, userRc=urc, rc=rc)
+#else
+       ! Call Finalize for the adjoint driver Component
+       call ESMF_GridCompFinalize(gcomp_ad, userRc=urc, rc=rc)
+#endif
+       if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+       if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
 
-     ! Run the TL driver Component
-     call ESMF_GridCompRun(gcomp_rp, phase=run_prep_phase, userRc=urc, rc=rc)
-     if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-     if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-     call ESMF_GridCompRun(gcomp_rp, userRc=urc, rc=rc)
-     if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-     if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-     call ESMF_GridCompRun(gcomp_rp, phase=run_post_phase, userRc=urc, rc=rc)
-     if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-     if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  !!! ***** Run the tangent linear model ***** !!!
+#ifdef USE_RUN_PREP_POST
+       ! Call RunPrep for the TL driver Component
+       call ESMF_GridCompRun(gcomp_rp, phase=run_prep_phase, userRc=urc, rc=rc)
+#else
+       ! Call Initialize for the TL driver Component
+       call ESMF_GridCompInitialize(gcomp_rp, userRc=urc, rc=rc)
+#endif
+       if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+       if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+       ! Call Run     for the TL driver Component
+       call ESMF_GridCompRun(gcomp_rp, userRc=urc, rc=rc)
+       if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+       if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+#ifdef USE_RUN_PREP_POST
+       ! Call RunPost for the TL driver Component
+       call ESMF_GridCompRun(gcomp_rp, phase=run_post_phase, userRc=urc, rc=rc)
+#else
+       ! Call Finalize for the TL driver Component
+       call ESMF_GridCompFinalize(gcomp_rp, userRc=urc, rc=rc)
+#endif
+       if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+       if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
 
   !!! ***** Check for convergence of solution ***** !!!
 
-  !!! ***** If converged; run post multiplication step (action of AD & TL) ***** !!!
+!!===================================================================================!!
+!!                             Post-Multiplication Step                              !!
+!!===================================================================================!!
 
-  enddo
+  enddo cg_iteration_loop
 
-  ! Finalize the driver Components
+!!===================================================================================!!
+!!                                   Finalize Run                                    !!
+!!===================================================================================!!
+
+#ifdef USE_RUN_PREP_POST
+  !! Finalize for the AD & TL driver Components
   call ESMF_GridCompFinalize(gcomp_ad, userRc=urc, rc=rc)
   if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
   if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
   call ESMF_GridCompFinalize(gcomp_rp, userRc=urc, rc=rc)
   if (ESMF_LogFoundError( rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
   if (ESMF_LogFoundError(urc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+#endif
 
-  ! Destroy the driver Components
+  !! Destroy the driver Components
   call ESMF_GridCompDestroy(gcomp_ad, rc=rc)
   if (ESMF_LogFoundError(rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
   call ESMF_GridCompDestroy(gcomp_rp, rc=rc)
   if (ESMF_LogFoundError(rc, PASSTHRU)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
 
-  ! Finalize ESMF
+  !! Finalize ESMF
   call ESMF_Finalize()
 
   !-----------------------------------------------------------------------------
